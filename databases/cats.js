27f92300d2be9fd3b80c02d98207edfbfd540de0
succatash <mstein1284@gmail.com>
2020-02-21 12:06:55 -0500

    fix deprecation problems

--- databases/cats.js
+++ databases/cats.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose");
 
+mongoose.connect("mongoose://localhost/cat_app");
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 //adding a new cat to Db, then retrieve all cats from db
