d986d80907a141d1b5fb77c0a64274ee9aa8b663
succatash <mstein1284@gmail.com>
2020-02-22 07:57:46 -0500

    DB update

--- databases/cats.js
+++ databases/cats.js
@@ -1,8 +1,44 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongoose://localhost/cat_app");
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
+mongoose
+  .connect("mongodb://localhost/cat_app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("DB Connected!"))
+  .catch(err => {
+    console.log(`DB Connection Error:${err.message}`);
+  });
+
+// var db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function() {
+//   // we're connected!
+// });
+
+const catSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  tempermeant: String
+});
+
+const Cat = mongoose.model("Cat", catSchema);
+
+let george = new Cat({ name: "george", age: 11, tempermeant: "grouchy" });
+
+george.save(
+  //callback function
+  (err, cat) => {
+    if (err) {
+      console.log("something went wrong");
+    } else {
+      console.log("We Just Save The Cates");
+      console.log(cat);
+    }
+  }
+);
 //adding a new cat to Db, then retrieve all cats from db
+
+// app.listen(port, () => {
+//   console.log("Example app listening on port 3000");
+// });
