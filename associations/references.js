0ddbdcc0bd10b462911e0035101e203b32d94c12
root <root@Succatash.localdomain>
2020-04-15 22:22:05 -0400

    deleted some extra stuff

--- associations/references.js
+++ associations/references.js
@@ -22,35 +22,35 @@ mongoose
 
 // this creates posts and pushes it to the user
 
-Post.create(
-	{
-		title: "how to Eat the best burger",
-		content: "YUM YUM YUM",
-	},
-	function (err, post) {
-		User.findOne({ email: "bob@gmail.com" }, function (err, foundUser) {
-			if (err) {
-				console.log(err);
-			} else {
-				foundUser.posts.push(post);
-				foundUser.save(function (err, data) {
-					if (err) {
-						console.log(err);
-					} else {
-						console.log(data);
-					}
-				});
-			}
-		});
-	}
-);
+// Post.create(
+// 	{
+// 		title: "how to Eat the best burger",
+// 		content: "YUM YUM YUM",
+// 	},
+// 	function (err, post) {
+// 		User.findOne({ email: "bob@gmail.com" }, function (err, foundUser) {
+// 			if (err) {
+// 				console.log(err);
+// 			} else {
+// 				foundUser.posts.push(post);
+// 				foundUser.save(function (err, data) {
+// 					if (err) {
+// 						console.log(err);
+// 					} else {
+// 						console.log(data);
+// 					}
+// 				});
+// 			}
+// 		});
+// 	}
+// );
 
-// User.findOne({ _id: "5e8e57635d65e56f438a36d9" })
-// 	.populate("posts")
-// 	.exec(function (err, user) {
-// 		if (err) {
-// 			console.log(err);
-// 		} else {
-// 			console.log(user);
-// 		}
-// 	});
+User.findOne({ email: "bob@gmail.com" })
+	.populate("posts")
+	.exec(function (err, user) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(user);
+		}
+	});
