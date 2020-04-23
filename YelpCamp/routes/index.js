f5f463e28d86f6e423a7b896de43fcbb06fcd4ef
root <root@Succatash.localdomain>
2020-04-23 15:18:31 -0400

    added a slider

--- YelpCamp/routes/index.js
+++ YelpCamp/routes/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const router = express.Router();
-
+const express = require("express"),
+	router = express.Router(),
+	middleware = require("../middleware");
 const passport = require("passport");
 const User = require("../models/user");
 
@@ -26,10 +26,12 @@ router.post("/register", function (req, res) {
 	const newUser = new User({ username: req.body.username });
 	User.register(newUser, req.body.password, function (err, user) {
 		if (err) {
+			req.flash("error", err.message);
 			console.log(err);
 			return res.render("register");
 		}
 		passport.authenticate("local")(req, res, function () {
+			req.flash("success", "Welcome to Yelpcamp");
 			res.redirect("/campgrounds");
 		});
 	});
@@ -62,17 +64,5 @@ router.get("/logout", function (req, res) {
 	req.flash("success", "Logged Out");
 	res.redirect("/");
 });
-
-/**
- * MiddleWare Functions
- */
-
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	} else {
-		res.redirect("/login");
-	}
-}
 
 module.exports = router;
